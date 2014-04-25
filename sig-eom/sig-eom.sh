bf9fd33e7a87a3752aa65b51a349cbba768b5bc2
signor <signor@gmx.com>
2014-04-25 11:53:53 +0930

    GNU General Public License, Script Header Update
    Updated all scripts with the GNU header disclaimer

--- sig-eom/sig-eom.sh
+++ sig-eom/sig-eom.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 ###############################################################################
 # crontab this script to run daily 5 minutes before midnight.
 # 55 23 * * *     /glftpd/bin/sig-eom.sh >/dev/null 2>&1
--- sig-purge_backup/sig-purge_backup.sh
+++ sig-purge_backup/sig-purge_backup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 ###############################################################################
 # this script copies user files from /ftp-data/users to /ftp-data/users_purged
 # for backup purposes before purge. the user is then fully purged from the site.
